bb3f191b70dec67b779c2f28ea2e499c5d434f42
David Rushton <david.rushton@firstdata.com>
2018-12-04 22:53:30 +0000

    Customised module psd1 and psm1 template files.

--- Source/Football-cli/Football-cli.psd1
+++ Source/Football-cli/Football-cli.psd1
@@ -70,7 +70,7 @@ Description = 'Command line access to football.  Back.  Of.  The.  Net.'
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = @(
-
+    'Get-FootballOption'
 )
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- Source/Football-cli/Football-cli.psm1
+++ Source/Football-cli/Football-cli.psm1
@@ -1,4 +1,28 @@
-# Implement your module commands in this script.
+#
+# Football-Cli Module
+#
+
+
+Set-StrictMode -Version 'Latest'
+
+
+# Get public, private and class definition files.
+$classes = @( Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 -ErrorAction SilentlyContinue )
+$public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1  -ErrorAction SilentlyContinue )
+$private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+# Dot source the files
+Foreach($import in @($classes + $private + $public))
+{
+    Try
+    {
+        . $import.FullName
+    }
+    Catch
+    {
+        Throw "Failed to import $($import.fullname): $_"
+    }
+}
 
 
 # Export only the functions using PowerShell standard verb-noun naming.
